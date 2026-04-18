7898a28036aea5010caaf895b95bb9fc0e7fba8c
Venkatesh <venkateshvadduri99@gmail.com>
2026-04-19 00:12:42 +0530

    Commit changes for TC01

--- src/test/java/Travelpage_TC01.java
+++ src/test/java/Travelpage_TC01.java
@@ -11,53 +11,53 @@ public class Travelpage_TC01 {
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        driver.findElement(By.id("autosuggest")).sendKeys("ind");
-        Thread.sleep(3000);
-        List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item']/a"));
+        WebDriver driver = new ChromeDriver(); //launching chrome browser
+        driver.manage().window().maximize();   //maximising the window
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");  //launching the application
+        driver.findElement(By.id("autosuggest")).sendKeys("ind"); //selecting the country option
+        Thread.sleep(3000); //waiting to load the page
+        List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item']/a")); //finding the all available options for the given country text
         for ( WebElement option : options){
            if( option.getText().equalsIgnoreCase("INDIA")){
                option.click();
                break;
            }
         }
-        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-        driver.findElement(By.cssSelector("a[value='HYD']")).click();
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click(); //click from departure element
+        driver.findElement(By.cssSelector("a[value='HYD']")).click(); // clicking on the from departure city
         Thread.sleep(1000);
-        driver.findElement(By.xpath( "(//a[@value='JAI'])[2]")).click();
-        driver.findElement(By.xpath("//a[@class='ui-state-default ui-state-active']")).click();
-        driver.findElement(By.id("divpaxinfo")).click();
+        driver.findElement(By.xpath( "(//a[@value='JAI'])[2]")).click(); // clicking on the To arraival city
+        driver.findElement(By.xpath("//a[@class='ui-state-default ui-state-active']")).click();  // clicking travel date
+        driver.findElement(By.id("divpaxinfo")).click(); // clicking on the passenger dropdown
         Thread.sleep(1000);
         for(int a=1;a<=4;a++){
-        driver.findElement(By.id("hrefIncAdt")).click();
+        driver.findElement(By.id("hrefIncAdt")).click();  //selecting adults count=5
         }
         Thread.sleep(1000);
         for(int b=1;b<=3;b++){
-            driver.findElement(By.id("hrefIncChd")).click();
+            driver.findElement(By.id("hrefIncChd")).click(); //selecting adults child=3
         }
         Thread.sleep(1000);
         for(int c=1;c<=2;c++){
-            driver.findElement(By.id("hrefIncInf")).click();
+            driver.findElement(By.id("hrefIncInf")).click(); //selecting adults infants=2
         }
-        driver.findElement(By.id("btnclosepaxoption")).click();
-        String textmsg = driver.findElement(By.id("divpaxinfo")).getText();
-        Assert.assertEquals(textmsg,"5 Adult, 3 Child, 2 Infant");
-        WebElement currencyoptions = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
-        Select currency = new Select(currencyoptions);
-        currency.selectByIndex(3);
+        driver.findElement(By.id("btnclosepaxoption")).click(); //clicking the done button
+        String textmsg = driver.findElement(By.id("divpaxinfo")).getText();  //getting the text msg from passenger dropdown
+        Assert.assertEquals(textmsg,"5 Adult, 3 Child, 2 Infant");  //validating the text msg from passenger dropdown
+        WebElement currencyoptions = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));  // clicking on the currency dropdown
+        Select currency = new Select(currencyoptions);  //providing the currency dropdown knowledge to select class
+        currency.selectByIndex(3);           //selecting  currency = USD
         String text = currency.getFirstSelectedOption().getText();
         Assert.assertEquals(text,"USD");
-        currency.selectByVisibleText("AED");
+        currency.selectByVisibleText("AED"); //selecting  currency = AED
         String text1 = currency.getFirstSelectedOption().getText();
         Assert.assertEquals(text1,"AED");
-        currency.selectByValue("INR");
+        currency.selectByValue("INR");       //selecting  currency = INR
         String text2 = currency.getFirstSelectedOption().getText();
         Assert.assertEquals(text2,"INR");
-        driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
-        driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
-        driver.quit();
+        driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();   //selecting  family and friends check box
+        driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();  //selecting  search button
+        driver.quit();   //closing the browser
 
 
 
